e645c02ccc0a597902393c1f0f5795b93bd8277f
Murad Sadigov <myurad5790@gmail.com>
2024-02-29 21:46:15 +0100

    Day 1 - 30 Days of JS

---
+++ LeetCode/30_Days_of_JavaScript/Counter_II.js
@@ -0,0 +1,38 @@
+/*
+Write a function createCounter. 
+It should accept an initial integer init. 
+It should return an object with three functions.
+
+The three functions are:
+
+increment() increases the current value by 1 and then returns it.
+decrement() reduces the current value by 1 and then returns it.
+reset() sets the current value to init and then returns it.
+*/
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function (init) {
+    let value = init;
+    return {
+        increment: function () {
+            return ++value;
+        },
+        decrement: function () {
+            return --value;
+        },
+        reset: function () {
+            value = init;
+            return value;
+        },
+    };
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
